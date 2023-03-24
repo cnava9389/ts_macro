c8e10e12fa27957859fbd20e5bd70f85b4e08787
christian nava <cnava9389@gmail.com>
2023-03-23 22:00:45 -0500

    checks function name and returnType

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,16 @@ use syn::{parse_macro_input, ItemFn};
 #[proc_macro_attribute]
 pub fn rs_route_ts(attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemFn);
-    println!("doing work");
-    (quote! {}).into()
+    let sig = &input.sig;
+    let ident = sig.ident.to_string();
+    let output = match &sig.output {
+        syn::ReturnType::Default => "undefined".to_owned(),
+        syn::ReturnType::Type(_, ty) => match ty.as_ref() {
+            syn::Type::Path(path) => path.path.segments.last().unwrap().ident.to_string(),
+            _ => "unknown".to_owned(),
+        },
+    };
+
+    println!("doing work {} {}\nother attrs {}", ident, output, attr);
+    (quote! {#input}).into()
 }
